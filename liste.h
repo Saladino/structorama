c35828db968d826276a989b0094e19d3510dba2b
Y4ke <yakedidudel@googlemail.com>
2013-06-15 16:18:47 +0200

    Update liste.h
    initListe erstellt jetzt einen Pointer mit Wert NULL.

--- liste.h
+++ liste.h
@@ -9,19 +9,22 @@ Verwaltet die Liste und beinhaltet die Pointer auf die CD Daten im CD Strukturmo
 #include "cd_info.h"
 
 typedef 
-  struct cdliste {
+	struct cdliste {
 		CD_Info * info;
 		struct cdliste * next;
 	} 
 CD_Liste;
    
-    void initListe (CD_Liste ** liste);
-	//Initialisierung mit NULL-Pointer
+    CD_Liste ** liste initListe (void);
+	//Initialisierung einer neuen Liste mit NULL-Pointer
 	void einfuegenListe (CD_Liste ** liste, CD_Info * cd);
 	//Koppelt Daten der CD mit Knoten der Liste
     void naechsteCD (CD_Liste ** liste);
 	//Springt ein next weiter in der Liste.
-	
+	void loescheCD (CD_Liste ** liste);
+	//Loescht die Daten der CD, den Knoten und überbrückt
+	//den vorherigen und den naechsten Knoten.
+
 
 
 #endif
