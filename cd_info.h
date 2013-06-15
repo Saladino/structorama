26751303e3755a20c57a240695997c494bdfec05
Y4ke <yakedidudel@googlemail.com>
2013-06-16 00:23:24 +0300

    Update cd_info.h

--- cd_info.h
+++ cd_info.h
@@ -7,11 +7,16 @@ sind die Daten der CD gespeichert.
 #ifndef CD_INFO
 #define CD_INFO
 
+//Includes
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 //Deklarationen
 
 typedef struct songliste
 	{
-		int nummer;
 		char songname[20];
 	}
 Songs;
@@ -28,6 +33,7 @@ typedef struct cd_info
 	{ 
 		char cd_name [21];
 		char interpret_name [21];
+		int song_anzahl;
 		Songs song[21];
 		char genre [21];
 		Datum erscheinung;
