30d87fc5989f671fbba374bd9c2c8ae8f66f6ae2
Y4ke <yakedidudel@googlemail.com>
2013-06-15 13:59:31 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,43 @@
+/*
+main.c:
+Beinhaltet das Menü, in dem der User durch drücken verschiedener
+die verschiedenen Funktionen des Programms starten
+kann. Hier findet der Aufruf der Funktionen statt.
+*/
+
+#include <stdio.h>
+#include "bearbeitung.h"
+
+int main(void)
+{
+
+//Variablendeklaration
+  
+	int ende = 0;
+
+
+//Überprüfung: Datei bereits vorhanden?
+
+
+
+//Benutzeroberfläche
+
+	while(ende==0)
+	{
+
+	
+
+
+
+	}
+
+//Herunterfahren des Programms
+
+
+
+
+
+
+
+	return 0;
+}
