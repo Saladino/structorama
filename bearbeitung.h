c399812a7771c230485e749b24e08fd388e0a749
Y4ke <yakedidudel@googlemail.com>
2013-06-16 19:45:11 +0200

    Update bearbeitung.h

--- bearbeitung.h
+++ bearbeitung.h
@@ -39,7 +39,7 @@ void sortiereSammlung (CD_Liste ** anker);
 //Sortierung ist hier falsch herum, aufgrund des Last-In First-Out Stacks.(!)
 //anker_alt wird dann auf anker_neu gesetzt.
 
-CD_Liste ** einlesenDatei (void);
+void einlesenDatei (CD_Liste ** anker);
 //initListe wird ausgeführt um neue Liste zu erstellen.
 //Aus der Datei werden dann die Elemente hinzugefügt.
 
